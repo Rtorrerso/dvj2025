f0daa59b4b901dee8a3d37189d950622d3a7e372
Ruben Torres <79935998+Rtorrerso@users.noreply.github.com>
2025-09-04 20:49:03 -0500

    actualizado 4sept
    4sept rev 1

---
+++ Assets/busqueda.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class busqueda : MonoBehaviour
+{
+    public Transform Parent;       // ahora es un Transform
+    public float rotationSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FPSController")
+        {
+            Debug.Log("Te encontre!!!");
+            //transform.parent.position = other.transform.position;
+            //transform.parent.LookAt(other.transform);
+            StartCoroutine(SmoothLookAt(other.transform));
+        }
+    }
+
+    private IEnumerator SmoothLookAt(Transform target)
+    {
+        Quaternion startRotation = Parent.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - Parent.position);
+
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime * rotationSpeed;
+
+            Parent.rotation = Quaternion.Slerp(startRotation, targetRotation, time);
+
+            yield return null;
+        }
+    }
+}
