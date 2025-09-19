c40f62d61c0294981379b6e1f493903a68bdbe75
Ruben Torres <79935998+Rtorrerso@users.noreply.github.com>
2025-09-18 21:15:48 -0500

    v8

--- Assets/Patrullaje.cs
+++ Assets/Patrullaje.cs
@@ -10,12 +10,14 @@ public class Patrullaje : MonoBehaviour
 
     private int locationIndex = 0;
     public NavMeshAgent agente;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         agente.GetComponent<NavMeshAgent>();
         inicializaPatrulla();
+        animator = GetComponent<Animator>();
     }
 
     void inicializaPatrulla()
@@ -33,7 +35,9 @@ public class Patrullaje : MonoBehaviour
             return;
         }
         agente.SetDestination(waypoint[locationIndex].position);
+        animator.CrossFadeInFixedTime("Walk", 0.1f);
         locationIndex = (locationIndex+1) % waypoint.Count;
+
     }
     // Update is called once per frame
     void Update()
